27911bec07e7b4cf7ea3b44c008e986c92c05346
Alex Vernacchia <vernak2539@users.noreply.github.com>
2023-06-29 20:01:06 +0100

    add rest of pm2.5 and pm10

--- cmd/main.go
+++ cmd/main.go
@@ -41,6 +41,8 @@ func (c *environmentMetricCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- c.metrics.Humidity
 	ch <- c.metrics.Temperature
 	ch <- c.metrics.PM1
+	ch <- c.metrics.PM25
+	ch <- c.metrics.PM10
 }
 
 func (c *environmentMetricCollector) Collect(ch chan<- prometheus.Metric) {
@@ -58,6 +60,8 @@ func (c *environmentMetricCollector) Collect(ch chan<- prometheus.Metric) {
 	ch <- prometheus.MustNewConstMetric(c.metrics.Humidity, prometheus.GaugeValue, humidity)
 	ch <- prometheus.MustNewConstMetric(c.metrics.Temperature, prometheus.GaugeValue, temp)
 	ch <- prometheus.MustNewConstMetric(c.metrics.PM1, prometheus.GaugeValue, float64(pm.Pm10Std))
+	ch <- prometheus.MustNewConstMetric(c.metrics.PM25, prometheus.GaugeValue, float64(pm.Pm25Std))
+	ch <- prometheus.MustNewConstMetric(c.metrics.PM10, prometheus.GaugeValue, float64(pm.Pm100Std))
 }
 
 var (
