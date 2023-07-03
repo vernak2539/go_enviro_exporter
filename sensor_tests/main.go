249cf6abcae425a736669ed193e31224eedb7e89
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2023-07-03 16:07:01 +0100

    try without i2c reg being done

--- sensor_tests/main.go
+++ sensor_tests/main.go
@@ -5,11 +5,7 @@ import (
 	"log"
 	"time"
 
-	"github.com/rubiojr/go-enviroplus/pms5003"
 	mics6814 "github.com/vernak2539/go_enviro_exporter/internal/devices"
-	"periph.io/x/conn/v3/i2c/i2creg"
-	"periph.io/x/conn/v3/physic"
-	"periph.io/x/devices/v3/bmxx80"
 	"periph.io/x/host/v3"
 )
 
@@ -20,27 +16,27 @@ func main() {
 	}
 
 	// Open a handle to the first available I²C bus:
-	bus, err := i2creg.Open("")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer bus.Close()
+	//bus, err := i2creg.Open("")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//defer bus.Close()
+	//
+	//dev, err := bmxx80.NewI2C(bus, 0x76, &bmxx80.DefaultOpts)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//defer dev.Halt()
+	//
+	//e := physic.Env{}
 
-	dev, err := bmxx80.NewI2C(bus, 0x76, &bmxx80.DefaultOpts)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer dev.Halt()
-
-	e := physic.Env{}
-
-	particulateMatterSensor, err := pms5003.New()
-	if err != nil {
-		panic(err)
-	}
-	go func() {
-		particulateMatterSensor.StartReading()
-	}()
+	//particulateMatterSensor, err := pms5003.New()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//go func() {
+	//	particulateMatterSensor.StartReading()
+	//}()
 
 	gasDev, err := mics6814.New()
 	if err != nil {
@@ -53,40 +49,40 @@ func main() {
 	}()
 
 	for {
-		pm := particulateMatterSensor.LastValue()
-		if err = dev.Sense(&e); err != nil {
-			log.Fatal(err)
-		}
+		//pm := particulateMatterSensor.LastValue()
+		//if err = dev.Sense(&e); err != nil {
+		//	log.Fatal(err)
+		//}
 
-		fmt.Print("    temp: ", e.Temperature.Celsius())
-		fmt.Println()
-
-		pressure := float64(e.Pressure) / float64(physic.KiloPascal/10)
-		fmt.Print("pressure: ", pressure)
-		fmt.Println()
-		fmt.Print("pressure: ", int64(e.Pressure))
-		fmt.Println()
-
-		humidity := float64(e.Humidity) / float64(physic.PercentRH)
-		fmt.Print("humidity: ", humidity)
-		fmt.Println()
-		fmt.Print("humidity: ", int64(e.Humidity))
-		fmt.Println()
-
-		fmt.Println("PM1.0 ug/m3 (ultrafine):                        ", pm.Pm10Std)
-		fmt.Println("PM2.5 ug/m3 (combustion, organic comp, metals): ", pm.Pm25Std)
-		fmt.Println("PM10 ug/m3 (dust, pollen, mould spores):        ", pm.Pm100Std)
-		fmt.Println("PM1.0 ug/m3 (atmos env):                        ", pm.Pm10Env)
-		fmt.Println("PM2.5 ug/m3 (atmos env):                        ", pm.Pm25Env)
-		fmt.Println("PM10 ug/m3 (atmos env):                         ", pm.Pm100Env)
-		fmt.Println("0.3um 1 0.1L air:                               ", pm.Particles3um)
-		fmt.Println("0.5um 1 0.1L air:                               ", pm.Particles5um)
-		fmt.Println("1.0um 1 0.1L air:                               ", pm.Particles10um)
-		fmt.Println("2.5um 1 0.1L air:                               ", pm.Particles25um)
-		fmt.Println("5um 1 0.1L air:                                 ", pm.Particles50um)
-		fmt.Println("10um 1 0.1L air:                                ", pm.Particles100um)
-		fmt.Println()
-		fmt.Println()
+		//fmt.Print("    temp: ", e.Temperature.Celsius())
+		//fmt.Println()
+		//
+		//pressure := float64(e.Pressure) / float64(physic.KiloPascal/10)
+		//fmt.Print("pressure: ", pressure)
+		//fmt.Println()
+		//fmt.Print("pressure: ", int64(e.Pressure))
+		//fmt.Println()
+		//
+		//humidity := float64(e.Humidity) / float64(physic.PercentRH)
+		//fmt.Print("humidity: ", humidity)
+		//fmt.Println()
+		//fmt.Print("humidity: ", int64(e.Humidity))
+		//fmt.Println()
+		//
+		//fmt.Println("PM1.0 ug/m3 (ultrafine):                        ", pm.Pm10Std)
+		//fmt.Println("PM2.5 ug/m3 (combustion, organic comp, metals): ", pm.Pm25Std)
+		//fmt.Println("PM10 ug/m3 (dust, pollen, mould spores):        ", pm.Pm100Std)
+		//fmt.Println("PM1.0 ug/m3 (atmos env):                        ", pm.Pm10Env)
+		//fmt.Println("PM2.5 ug/m3 (atmos env):                        ", pm.Pm25Env)
+		//fmt.Println("PM10 ug/m3 (atmos env):                         ", pm.Pm100Env)
+		//fmt.Println("0.3um 1 0.1L air:                               ", pm.Particles3um)
+		//fmt.Println("0.5um 1 0.1L air:                               ", pm.Particles5um)
+		//fmt.Println("1.0um 1 0.1L air:                               ", pm.Particles10um)
+		//fmt.Println("2.5um 1 0.1L air:                               ", pm.Particles25um)
+		//fmt.Println("5um 1 0.1L air:                                 ", pm.Particles50um)
+		//fmt.Println("10um 1 0.1L air:                                ", pm.Particles100um)
+		//fmt.Println()
+		//fmt.Println()
 
 		fmt.Printf("Oxidising: %.2f\n", gasDev.LastValue().Oxidising)
 		fmt.Printf("Reducing:  %.2f\n", gasDev.LastValue().Reducing)
